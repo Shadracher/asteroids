793a3be0ed45714b25ee488c2454b1b71adc09b8
Shadracher <jasdgoearin@hotmail.com>
2026-06-05 00:27:02 +0000

    added spaceship player and rotation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(
@@ -8,13 +9,21 @@ def main():
     )
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2, radius =  PLAYER_RADIUS)
+
     while True:
         log_state()
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    fill("black")
-    display.flip()
+        dt = clock.tick(60) / 1000
+    
 
 if __name__ == "__main__":
     main()
